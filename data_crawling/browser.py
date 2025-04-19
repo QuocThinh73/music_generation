5dcad99acad62c43e9217f16b01c4dc77a9ca302
farolnguyen <farolnguyen824@gmail.com>
2025-04-20 01:32:59 +0700

    Update browser.py

--- data_crawling/browser.py
+++ data_crawling/browser.py
@@ -3,10 +3,10 @@ from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.chrome.options import Options as ChromeOptions
 from selenium.webdriver.support.ui import WebDriverWait
 
-import config
+from data_crawling.config import CHROME_DRIVER_PATH, IMPLICIT_WAIT, WEB_DRIVER_DELAY
 
 def init_driver():
-    service = ChromeService(executable_path=config.CHROME_DRIVER_PATH)
+    service = ChromeService(executable_path=CHROME_DRIVER_PATH)
     options = ChromeOptions()
     options.add_argument("--headless")
     options.add_argument("--no-sandbox")
@@ -14,6 +14,6 @@ def init_driver():
     options.add_argument("window-size=1920x1080")
 
     driver = webdriver.Chrome(service=service, options=options)
-    driver.implicitly_wait(config.IMPLICIT_WAIT)
-    wait = WebDriverWait(driver, config.WEB_DRIVER_DELAY)
+    driver.implicitly_wait(IMPLICIT_WAIT)
+    wait = WebDriverWait(driver, WEB_DRIVER_DELAY)
     return driver, wait
