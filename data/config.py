5a46a8259f7f35e6651db183eb0acb6c1c820f51
QuocThinh73 <ttqthinh2004@gmail.com>
2025-04-20 23:39:11 +0700

    Modify dir

--- data/config.py
+++ data/config.py
@@ -1,11 +1,13 @@
 import os
-
+from pathlib import Path
 
 SAMPLE_RATE = 22050
-DURATION = 3  # seconds
+DURATION = 3
 N_MELS = 256
 
-AUDIO_DIR = os.path.join("data", "crawled_data", "audio")
-JSON_DIR = os.path.join("data", "crawled_data")
+CURRENT_FILE = Path(__file__).resolve()
+REPO_ROOT = CURRENT_FILE.parent.parent
+AUDIO_DIR = os.path.join(REPO_ROOT, "crawled_data", "audio")
+JSON_DIR = os.path.join(REPO_ROOT, "crawled_data")
 
 TEST_AMOUNT = 32
